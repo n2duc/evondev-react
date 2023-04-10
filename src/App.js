44d86c5591768cec3bbe5ee935776a2197c73242
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2023-04-10 10:08:33 +0700

    movie search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.scss";
-import Form from "./components/form/Form";
+import MovieSearch from "./components/movieSearch/MovieSearch";
+// import Form from "./components/form/Form";
 // import MainSideBar from "./components/hookExample/MainSideBar";
 
 function App() {
     return (
         <div>
-            <Form />
+            <MovieSearch />
         </div>
     );
 }
---
+++ src/components/movieSearch/MovieSearch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// https://api.themoviedb.org/3/movie/550?api_key=891b4b911754b82a36212a38d99888dc
+
+const MovieSearch = () => {
+    return (
+        <div>MovieSearch</div>
+    )
+}
+
+export default MovieSearch
